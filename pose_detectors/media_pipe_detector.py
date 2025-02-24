2be6fe7de10a9232392d3a0e52fcabca2a3b739d
samsniderheld <samsniderheld@gmail.com>
2025-02-24 16:36:52 -0500

    Update media_pipe_detector.py

--- pose_detectors/media_pipe_detector.py
+++ pose_detectors/media_pipe_detector.py
@@ -3,15 +3,17 @@ import numpy as np
 import os
 import requests
 from PIL import Image
-from pose_detectors import PoseDetector
+from .pose_detector import PoseDetector
 
-
-from mediapipe.tasks.vision import poseLandmarkerResult
+from mediapipe.tasks.python.vision.pose_landmarker import PoseLandmarkerResult
+from mediapipe.python._framework_bindings.image import Image
 
 class MediaPipeDetector(PoseDetector):
     def __init__(self):
-        self.load_model()
         self.model_path = "pose_landmarker_heavy.task"
+        self.check_and_download_model()
+        self.load_model()
+        
 
     def check_and_download_model(self):
         if not os.path.exists(self.model_path):
@@ -34,22 +36,27 @@ class MediaPipeDetector(PoseDetector):
             base_options=self.BaseOptions(model_asset_path=self.model_path),
         running_mode=self.VisionRunningMode.IMAGE)
 
-    def load_image_path(self, image_path):
+    def load_image_path(self, image_path) -> Image:
         # Load the input image from an image file.
         mp_image = mp.Image.create_from_file(image_path)
         return mp_image
     
-    def load_image_np_array(self, numpy_image):
+    def load_image_np_array(self, numpy_image) -> Image:
         # Load the input image from a numpy array.
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=numpy_image)
         return mp_image
 
-    def detect_poses(self, image: Image) -> poseLandmarkerResult:
-        image = np.array(image)
-        mp_image = self.load_image_np_array(image)
+    def detect_poses(self, image_or_path) -> PoseLandmarkerResult:
+        if isinstance(image_or_path, Image):
+            image = np.array(image_or_path)
+            mp_image = self.load_image_np_array(image)
+        elif isinstance(image_or_path, str):
+            mp_image = self.load_image_path(image_or_path)
+        else:
+            raise ValueError("Input must be a PIL Image or a file path")
 
         with self.PoseLandmarker.create_from_options(self.options) as landmarker:
-        # The landmarker is initialized. Use it here.
+            # The landmarker is initialized. Use it here.
             pose_landmarker_result = landmarker.detect(mp_image)
         
         return pose_landmarker_result
