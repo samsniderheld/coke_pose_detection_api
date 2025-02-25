c715912a418b56af337ce0ce3b8f53ba60594f47
samsniderheld <samsniderheld@gmail.com>
2025-02-25 10:59:57 -0500

    Update media_pipe_detector.py

--- pose_detectors/media_pipe_detector.py
+++ pose_detectors/media_pipe_detector.py
@@ -7,6 +7,8 @@ from .pose_detector import PoseDetector
 
 from mediapipe.tasks.python.vision.pose_landmarker import PoseLandmarkerResult
 from mediapipe.python._framework_bindings.image import Image
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
 
 class MediaPipeDetector(PoseDetector):
     def __init__(self):
@@ -45,8 +47,28 @@ class MediaPipeDetector(PoseDetector):
         # Load the input image from a numpy array.
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=numpy_image)
         return mp_image
+    
+    def draw_landmarks_on_image(rgb_image, detection_result):
+        pose_landmarks_list = detection_result.pose_landmarks
+        annotated_image = np.copy(rgb_image)
 
-    def detect_poses(self, image_or_path) -> PoseLandmarkerResult:
+        # Loop through the detected poses to visualize.
+        for idx in range(len(pose_landmarks_list)):
+            pose_landmarks = pose_landmarks_list[idx]
+
+            # Draw the pose landmarks.
+            pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+            pose_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+            ])
+            solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            pose_landmarks_proto,
+            solutions.pose.POSE_CONNECTIONS,
+            solutions.drawing_styles.get_default_pose_landmarks_style())
+        return annotated_image
+
+    def detect_poses(self, image_or_path) -> list[PoseLandmarkerResult, Image]:
         if isinstance(image_or_path, Image):
             image = np.array(image_or_path)
             mp_image = self.load_image_np_array(image)
@@ -58,6 +80,10 @@ class MediaPipeDetector(PoseDetector):
         with self.PoseLandmarker.create_from_options(self.options) as landmarker:
             # The landmarker is initialized. Use it here.
             pose_landmarker_result = landmarker.detect(mp_image)
+
+        rendered_image = self.draw_landmarks_on_image(mp_image, pose_landmarker_result)
         
-        return pose_landmarker_result
+        return [pose_landmarker_result, rendered_image]
+    
+    
     
